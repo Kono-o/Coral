a0d6653dbe051a6099109972d56b98af5e37aaae
Kono <75730503+Kono-o@users.noreply.github.com>
2024-02-27 23:39:30 +0530

    nbt reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+extern crate nbt;
+extern crate serde_json;
+
+use nbt::Blob;
+use std::fs::File;
+use std::path::Path;
+
+use nbt::Result;
+
+fn run() -> Result<()>
+{
+    let path: &Path = Path::new(".mcs/level.dat");
+    let mut file = File::open(path).unwrap();
+    
+    println!("file > {:?}", file);
+    
+    println!("NBT Contents");
+    let blob = Blob::from_gzip_reader(&mut file).unwrap();
+    println!("{:?}", blob);
+    
+    return Ok(());
+}
+
+fn main()
+{
+    print!("{:?}", run());
 }
