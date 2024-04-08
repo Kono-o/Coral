87340066a2dc283fbea5e6814fc332e705669f07
Kono <75730503+Kono-o@users.noreply.github.com>
2024-04-09 03:23:14 +0530

    version

--- cogui/src/main.rs
+++ cogui/src/main.rs
@@ -2,11 +2,13 @@ use std::path::PathBuf;
 use coren::*;
 
 fn main() {
+  println!("LINUX");
   match packs::get_packs(&PathBuf::from("/home/kono/.minecraft/resourcepacks")) {
     Ok(..) => println!("worked"),
     Err(err) => println!("{:?}", err)
   }
-  match packs::get_packs(&PathBuf::from("./res")) {
+  println!("WINDOWS");
+  match packs::get_packs(&PathBuf::from("C:/Users/aryaa/AppData/Roaming/.minecraft/resourcepacks")) {
     Ok(..) => println!("worked"),
     Err(err) => println!("{:?}", err)
   }
