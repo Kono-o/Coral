94a770f46f9627eafe4d97b8bfbf3535223ab5ff
Kono <75730503+Kono-o@users.noreply.github.com>
2024-04-10 19:57:44 +0530

    idk

--- cogui/src/main.rs
+++ cogui/src/main.rs
@@ -1,15 +1,20 @@
-use std::path::PathBuf;
 use coren::*;
+use std::path::PathBuf;
 
 fn main() {
-  println!("LINUX");
-  match packs::get_packs(&PathBuf::from("/home/kono/.minecraft/resourcepacks")) {
-    Ok(..) => println!("worked"),
-    Err(err) => println!("{:?}", err)
-  }
-  println!("WINDOWS");
-  match packs::get_packs(&PathBuf::from("C:/Users/aryaa/AppData/Roaming/.minecraft/resourcepacks")) {
-    Ok(..) => println!("worked"),
-    Err(err) => println!("{:?}", err)
-  }
+    println!("LINUX");
+    match packs::get(&PathBuf::from("/home/kono/.minecraft/resourcepacks")) {
+        Ok(..) => println!("worked"),
+        Err(err) => println!("{:?}", err),
+    }
+    println!("WINDOWS");
+    match packs::get(&PathBuf::from(
+        "C:/Users/aryaa/AppData/Roaming/.minecraft/resourcepacks",
+    )) {
+        Ok(..) => println!("worked"),
+        Err(err) => println!("{:?}", err),
+    }
+    let color = atlas::Atlas::new(atlas::Map::Color, 2048);
+    color.save("./pack","TEST");
+
 }
